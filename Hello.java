a8dc58f61e3056319a688919080ed7a121c5bbba
Olivia Gette <omgette@mtu.edu>
2026-02-24 11:01:36 -0500

    Change output message and introduce NullPointerException
    Updated output message and added a potential NullPointerException.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-        int x = 1/1;
+        System.out.println("About to crash...");
+        String s = null;
+        System.out.println(s.length()); // NullPointerException at runtime
     }
 }
